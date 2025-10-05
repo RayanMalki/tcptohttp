d31a897696ae17ece18b13d5ff94ef435ea01a97
RayanMalki <rayanmalki54@gmail.com>
2025-10-05 02:11:39 -0400

    intégration du parseur HTTP dans le serveur TCP
    - Suppression de la logique getLinesChannel au profit du parsing HTTP
    - Intégration de la fonction RequestFromReader pour lire et analyser les requêtes
    - Affichage formaté de la ligne de requête (méthode, cible, version)
    - Amélioration de la gestion d’erreurs et du flux de connexion

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,58 +1,18 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"log"
 	"net"
+
+	"github.com/RayanMalki/tcptohttp/internal/request"
 )
-
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	ch := make(chan string)
-
-	go func() {
-		defer f.Close()
-		defer close(ch)
-
-		str := ""
-
-		for {
-			data := make([]byte, 8)
-			n, err := f.Read(data)
-
-			if err == io.EOF {
-				break
-			}
-			if err != nil {
-				panic(err)
-			}
-
-			data = data[:n]
-
-			if i := bytes.IndexByte(data, '\n'); i != -1 {
-				str += string(data[:i])
-				ch <- str
-				str = ""
-				data = data[i+1:]
-			}
-
-			str += string(data)
-		}
-
-		if len(str) != 0 {
-			ch <- str
-		}
-	}()
-
-	return ch
-}
 
 const port = ":42069"
 
 func main() {
 
-	listener, err := net.Listen("tcp4", port)
+	listener, err := net.Listen("tcp", port)
 
 	if err != nil {
 		log.Fatalf("error listening for TCP traffic: %s", err.Error())
@@ -68,14 +28,20 @@ func main() {
 		}
 		fmt.Println("Accepted connection from", conn.RemoteAddr())
 
-		linesChan := getLinesChannel(conn)
-
-		for line := range linesChan {
-			fmt.Println(line)
+		req, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Printf("Bad request from %s: %s", conn.RemoteAddr(), err)
+			continue
 		}
+
+		fmt.Println("Request line:")
+		fmt.Println("-Method: " + req.RequestLine.Method)
+		fmt.Println("-Target: " + req.RequestLine.RequestTarget)
+		fmt.Println("-Version: " + req.RequestLine.HttpVersion)
 
 		fmt.Println("Connection to", conn.RemoteAddr(), "closed")
 
 	}
 
 }
+

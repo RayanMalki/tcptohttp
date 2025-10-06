0e2d89d7df5f1cbb355e330af574f5b08990c247
RayanMalki <rayanmalki54@gmail.com>
2025-10-06 02:58:59 -0400

    implémentation complète du parsing des en-têtes HTTP
    - Ajout de la structure Headers et de la fonction NewHeaders pour initialiser la map
    - Implémentation de la fonction Parse permettant de lire et stocker les paires clé/valeur depuis un flux de données
    - Ajout de la fonction parseHeader pour gérer la validation du format "clé: valeur"
    - Gestion des cas particuliers : fin des en-têtes (CRLF vide), absence de données suffisantes et format malformé

---
+++ internal/headers/headers.go
@@ -0,0 +1,70 @@
+package headers
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
+
+type Headers map[string]string
+
+var rn = []byte("\r\n")
+
+func NewHeaders() Headers {
+	return map[string]string{}
+}
+func IsKeyCharValid(s string) bool {
+	for _, r := range s {
+		if (r >= 'a' && r <= 'z') ||
+			(r >= 'A' && r <= 'Z') ||
+			(r >= '0' && r <= '9') ||
+			strings.ContainsRune("!#$%&'*+-.^_`|~", r) {
+			continue
+		}
+		return false
+	}
+	return true
+}
+
+func parseHeader(fieldLine []byte) (string, string, error) {
+	parts := bytes.SplitN(fieldLine, []byte(":"), 2)
+	if len(parts) != 2 {
+		return "", "", fmt.Errorf("malformed field name")
+
+	}
+	name := parts[0]
+	value := bytes.TrimSpace(parts[1])
+	if bytes.HasSuffix(name, []byte(" ")) {
+		return "", "", fmt.Errorf("malformed field name")
+
+	}
+	return string(name), string(value), nil
+
+}
+func (h Headers) Parse(data []byte) (n int, done bool, err error) {
+	read := 0
+	done = false
+	for {
+		idx := bytes.Index(data[read:], rn)
+		if idx == -1 {
+			break
+
+		}
+		//Empty header
+		if idx == 0 {
+			done = true
+			read += len(rn)
+			break
+		}
+
+		name, value, err := parseHeader(data[read : read+idx])
+		if err != nil {
+			return 0, false, err
+
+		}
+		read += idx + len(rn)
+		h[name] = value
+
+	}
+	return read, done, nil
+}
